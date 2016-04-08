58969f14fb1982f9f68e7e7b7ad976324873cfd1
Carlos Carrasco <carlosc@gamehouse.com>
2016-04-08 10:30:02 +0200

    Configure winrm

---
+++ config-winrm.vmextension.js
@@ -0,0 +1,25 @@
+var objService = GetObject ('winmgmts:{impersonationLevel=impersonate}!\\\\.\\root\\default');
+var objReg = objService.Get ('StdRegProv');
+var objShell = new ActiveXObject ('WScript.Shell');
+
+var objMethod = objReg.Methods_.Item('EnumKey'); 
+var objParamsIn = objMethod.InParameters.SpawnInstance_(); 
+objParamsIn.hDefKey = 0x80000002; 
+objParamsIn.sSubKeyName = 'SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\NetworkList\\Profiles'; 
+
+var objParamsOut = objReg.ExecMethod_ (objMethod.Name, objParamsIn); 
+
+if (objParamsOut.ReturnValue === 0) {
+  if (objParamsOut.sNames != null) {
+    var a = objParamsOut.sNames.toArray();
+    for (var i = 0; i < a.length; ++i) {
+      var key = 'HKLM\\' + objParamsIn.sSubKeyName + '\\' + a[i] + '\\Category';
+      objShell.RegWrite (key,  1, 'REG_DWORD'); 
+    }
+  }
+}
+
+objShell.Run ('winrm quickconfig -quiet', 1, true);
+objShell.Run ('winrm set winrm/config/service/auth @{Basic="true"}', 1, true);
+objShell.Run ('winrm set winrm/config/service @{AllowUnencrypted="true"}', 1, true);
+objShell.Run ('netsh advfirewall firewall add rule name="WinRM-HTTP" dir=in localport=5985 protocol=TCP action=allow', 1, true);
